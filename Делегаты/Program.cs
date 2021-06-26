926573eeac0436298ebdf9b2ea1073fc50c4e98f
danilsocol <danilsocol-2002@mail.ru>
2021-06-26 10:01:09 +0500

    1

---
+++ Делегаты/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Делегаты
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> f = (x, y) => x + y;
+            Console.WriteLine(f(1, 2));
+
+            Action<int> g = (x) => Console.WriteLine(x);
+            g(3);
+        }
+    }
+}
